a683a04243797234c329e9f754b8508f93841e39
fyodoregor <44232459+fyodoregor@users.noreply.github.com>
2018-12-29 12:53:47 +0300

    Update pysendpulse.py

--- pysendpulse/pysendpulse.py
+++ pysendpulse/pysendpulse.py
@@ -322,7 +322,7 @@ class PySendPulse:
             return self.__handle_error("Emails list can't be converted by JSON library")
         return self.__handle_result(self.__send_request('addressbooks/{}/emails'.format(id), 'DELETE', {'emails': emails}))
 
-    def get_emails_stat_by_campaigns(self, campaign, emails):
+    def get_emails_stat_by_campaigns(self, campaign, email):
         """ Get campaigns statistic for list of emails
 
         @param emails: list of emails ['test_1@test_1.com', ..., 'test_n@test_n.com']
@@ -336,7 +336,7 @@ class PySendPulse:
         except:
             logging.debug("Emails: {}".format(emails))
             return self.__handle_error("Emails list can't be converted by JSON library")
-        return self.__handle_result(self.__send_request('campaigns/{}/emails/{}', 'GET', {'campaigns': campaign, 'emails': emails}))
+        return self.__handle_result(self.__send_request('campaigns/{}/emails/{}'.format(campaign, email), 'GET')
 
     # ------------------------------------------------------------------ #
     #                        EMAIL  CAMPAIGNS                            #
